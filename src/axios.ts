24b0289872175f2622f806df62da7895864b59ff
BogAlex4ndr <bog.alex4ndr@gmail.com>
2023-03-12 12:27:08 +0200

    fix bug

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://photo-gallery-server.onrender.com/',
+  baseURL: 'https://photo-gallery-server.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
